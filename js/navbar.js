c0d97068c32028e54fba611d2415b5821362b122
Ekjot Kaur <ekjotkaurk13@gmail.com>
2021-01-18 19:14:57 +0530

    third commit

---
+++ js/navbar.js
@@ -0,0 +1,53 @@
+window.onresize = function(){ location.reload(); }
+
+if(screen.width < 780) {
+  document.getElementById("navbtn").style.display = "flex";
+  document.getElementById("navlinks").style.display = "none";
+  document.getElementById("mynavbar").style.justifyContent = "flex-start";
+  document.getElementById('brandname').style.paddingLeft = "10px";
+  document.getElementById('fixedside').style.display = "flex";
+} else {
+  document.getElementById("navbtn").style.display = "none";
+  document.getElementById("navlinks").style.display = "block";
+  document.getElementById("mynavbar").style.justifyContent = "space-between";
+  document.getElementById('brandname').style.paddingLeft = "50px";
+  document.getElementById('fixedside').style.display = "none";
+}
+
+const openNav = () => {
+  const width = document.getElementById("sidenav").style.width;
+  if(width == "0px" || !width){
+    document.getElementById("sidenav").style.width = "180px";
+    document.getElementById("sidenav").style.display = "flex";
+  } else {
+    document.getElementById("sidenav").style.width = "0px";
+    document.getElementById("sidenav").style.display = "none";
+  }
+}
+
+// console.log(document.body.scrollTop);
+
+
+// var nav = $("#mynavbar");
+
+// $(document).scroll(function(){
+//      if($(this).scrollTop() > 100)
+//     {
+//         // alert("hello");
+//         nav.addClass("navbar-scroll"); 
+//         // img.css({
+//         //     width: "50px",
+//         //     height: "50px",
+//         //     margin: "10px"
+//         // });
+//     }
+//     else
+//     {
+//         nav.removeClass("navbar-scroll"); 
+//         // img.css({
+//         //     width: "100px",
+//         //     height: "100px",
+//         //     margin: "30px"
+//         // });
+//     }
+// });
---
+++ js/skills.js
@@ -0,0 +1,25 @@
+const widthOfCol = $(".skill").width();
+
+const skillImg = $(".skill-figure");
+
+const lanWidth = $("#lang").width();
+
+let margin;
+let padding = 0;
+
+
+
+if(screen.width >= 992) {
+  margin = (lanWidth - widthOfCol * 4) / 8;
+} else if(screen.width >= 768){
+  margin = (lanWidth - widthOfCol * 7) / 14;
+} else if(screen.width >= 576) {
+  margin = (lanWidth - widthOfCol * 4) / 8;
+} else {
+  margin = (lanWidth - widthOfCol * 3) / 6;
+}
+if(margin<2){
+  padding = 10;
+}
+skillImg.css({width: widthOfCol-10, height: 7/6*widthOfCol});
+$('.skill').css({margin: margin, padding: padding})
