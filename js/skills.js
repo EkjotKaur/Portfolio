238d2e8d654a5f327a7c59d0d357db396f15965a
Ekjot Kaur <ekjotkaurk13@gmail.com>
2021-02-07 16:53:48 +0530

    about skills

--- js/skills.js
+++ js/skills.js
@@ -9,17 +9,17 @@ let padding = 0;
 
 
 
-if(screen.width >= 992) {
-  margin = (lanWidth - widthOfCol * 4) / 8;
-} else if(screen.width >= 768){
-  margin = (lanWidth - widthOfCol * 7) / 14;
-} else if(screen.width >= 576) {
-  margin = (lanWidth - widthOfCol * 4) / 8;
-} else {
-  margin = (lanWidth - widthOfCol * 3) / 6;
-}
-if(margin<2){
-  padding = 10;
-}
-skillImg.css({width: widthOfCol-10, height: 7/6*widthOfCol});
-$('.skill').css({margin: margin, padding: padding})
+// if(screen.width >= 992) {
+//   margin = (lanWidth - widthOfCol * 4) / 8;
+// } else if(screen.width >= 768){
+//   margin = (lanWidth - widthOfCol * 7) / 14;
+// } else if(screen.width >= 576) {
+//   margin = (lanWidth - widthOfCol * 4) / 8;
+// } else {
+//   margin = (lanWidth - widthOfCol * 3) / 6;
+// }
+// if(margin<2){
+//   padding = 10;
+// }
+// skillImg.css({width: widthOfCol-10, height: 7/6*widthOfCol});
+// $('.skill').css({margin: margin, padding: padding})
