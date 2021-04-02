b6601d5d6cebc4e33830ab0d2b81f46a0ae93187
Ekjot Kaur <ekjotkaurk13@gmail.com>
2021-04-02 14:15:14 +0530

    project and typewriting

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+// var myModal = document.getElementById('exampleModal')
+// var myInput = document.getElementById('modalButton')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus();
+// })
